82e203a23f0d72ab19a47ce37fe49c31e18ee6f6
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-07-27 06:38:27 +0000

    [Add] edit,index,shoeのviewを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'books/new'
-  get 'books/edit' => 'books#:id'
-  post 'books' => 'books#create'
-  get 'books/show'
-  get 'books/index'
-  delete 'books/destroy'
+  get 'top' => 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
