40df762456f325b26553931ed1e3e32b6e3233bf
EC2 Default User <ec2-user@ip-172-31-24-247.ap-northeast-1.compute.internal>
2022-07-29 01:38:09 +0000

    [Add]create,updateアクションのbookに＠を追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book), notice:'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
@@ -23,9 +27,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice:'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -35,8 +42,8 @@ class BooksController < ApplicationController
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title,:body)
   end
-
 end
